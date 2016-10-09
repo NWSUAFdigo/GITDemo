9e280392c6f778c8e31a2ca20474e4ade824bcc5
NWSUAF <wudi9426@163.com>
2016-10-09 08:59:25 +0800

    初始化项目完成

---
+++ test2/test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by wudi on 16/10/9.
+//  Copyright © 2016年 wudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test2/test2/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  test2
+//
+//  Created by wudi on 16/10/9.
+//  Copyright © 2016年 wudi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"初始化项目完成");
+}
+
+
+@end
